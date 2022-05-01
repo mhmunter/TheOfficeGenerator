dbf8b0c441ae59e0fce39a0ae3bb4aeb33001d70
mhmunter <mark.munter1@gmail.com>
2022-05-01 16:48:48 -0500

    added update function, and useage video

--- server.js
+++ server.js
@@ -10,34 +10,35 @@ name: "choice",
 choices: ["all departments", "view all roles","view all employees", "add a department","add a role", "add an employee", "update an employee role"],
 message: "what do you want too do?"
 
-  })
-  .then(function(value){
-    switch (value.choice){
-      case 'all departments':
-        viewAllDepartments()
+})
+.then(function(value){
+  switch (value.choice){
+    case 'all departments':
+      viewAllDepartments()
+      break;
+    case 'view all roles':
+      viewAllRoles()
+      break;
+    case 'view all employees':
+      viewAllEmployees()
+      break;
+    case 'add a department':
+      createDepartment()
+      break;
+      case 'add a role':
+        addRole()
         break;
-      case 'view all roles':
-        viewAllRoles()
-        break;
-      case 'view all employees':
-        viewAllEmployees()
-        break;
-      case 'add a department':
-        createDepartment()
-        break;
-        case 'add a role':
-          addRole()
+        case 'add an employee':
+          addEmployee()
           break;
-          case 'add employee':
-            addEmployee()
+          case 'update an employee role':
+            updateRole()
             break;
-            case 'update employee': 
-            updateEmployee()
 default: 
 console.log(value.choice, "There is no case for this option.")
 
-    }
-  })
+  }
+})
 }
 
 db.connect(function(err){
@@ -119,28 +120,81 @@ viewAllRoles()
 function addEmployee() {
   inquirer.prompt([{
     type: 'input',
-    name: 'firstName',
+    name: 'first_name',
     message: 'What is the first name of the added employee?'
   }, {
     type: "input",
-    name: 'lastName',
+    name: 'last_name',
     message: 'What is the last name of added emplyoee?'
-  }, {
-    type: 'list',
-    name: 'department_id',
-    message: 'What is department is the added employee goig into?',
-    choices: [{ name: "sales", value: 1 }, { name: "warehouse", value: 2 }, { name: "accounting", value: 3 }]
-  }, {
+  },{
     type: 'list',
     name: 'role_id',
     message: 'What is department is the added employee goig into?',
     choices: [{ name: "manager", value: 1 }, { name: "midlevel", value: 2 }, { name: "lead", value: 3 }]
-  }
+  },
 ])
 .then(function (value) {
-    db.query("INSERT INTO employee (firstName, lastName, department_id, role_id) VALUES (?,?,?,?)", [value.firstName, value.lastName, value.department_id, value.role_id], function (err, data) {
+    db.query("INSERT INTO employee (first_name, last_name, role_id) VALUES (?,?,?)", [value.first_name, value.last_name, value.role_id], function (err, data) {
       if (err) throw err
       viewAllRoles()
     })
   })
 }
+
+
+
+
+// function updateRole(){
+// inquirer.prompt([{
+//   type: "input",
+//   name: "updateName",
+//   Message: "What is the Employee's name?"
+// },{
+// type: "input",
+// name: "updateRole",
+// Message: "what is their new role?"
+// }])
+// .then(function (value) {
+//   db.query("UPDATE employee SET role_id = WHERE id = ?", {
+//     if (err) throw err
+//     viewAllRoles()
+//   })
+// })
+// }
+
+// function getRoleId(roleName) {
+//   let query = "SELECT * FROM role WHERE role.title=?";
+//   let args = [roleName];
+//   const rows = await db.query(query, args);
+//   return rows[0].id;
+// }
+
+
+
+function updateRole() {
+  //const newRole = db.query('SELECT * from employee LEFT JOIN role ON employee.role_id = role.id')
+  //let joinEmplyoeeNames = db.query(`SELECT CONCAT(first_name , " ", last_name) AS 'Fullname' FROM employee;`)
+  //console.log(joinEmplyoeeNames)
+  inquirer.prompt([{
+    type: 'list',
+    name: 'updatedRole',
+    message: 'What new role does employee have?',
+    choices: [{ name: "manager", value: 1 }, { name: "midlevel", value: 2 }, { name: "lead", value: 3 }]
+  },{
+    type: 'input',
+    name: 'first_name',
+    message: 'What is the first name of the updated employee?'
+  },{
+    type: 'input',
+    name: 'last_name',
+    message: 'What is the last name of the updated employee?'
+  }
+])
+.then(function (value) {
+    db.query("UPDATE employee SET role_id = ? where first_name = ? and last_name = ?",[value.updatedRole, value.first_name, value.last_name], function (err, data) {
+      if (err) throw err
+      viewAllRoles()
+    })
+  })
+}
+//"UPDATE employee SET role_id = ? WHERE id = ?",
